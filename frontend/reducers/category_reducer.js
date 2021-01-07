429a1e4cbf460281686ae0568506c65653ab78d9
eric2523@gmail.com <eric2523@gmail.com>
2021-01-07 00:29:14 -0800

    add some annotations

--- frontend/reducers/category_reducer.js
+++ frontend/reducers/category_reducer.js
@@ -2,7 +2,8 @@ const defaultState = {
   byId: {},
   allIds: []
 }
-
+// No actions to dispatch right now. In the future we can make it so that user can create categories 
+// example action.type: RECEIVE_CATEGORY
 const categoryReducer = (state = defaultState, action) => {
   return state;
 }
--- frontend/reducers/reward_reducer.js
+++ frontend/reducers/reward_reducer.js
@@ -2,7 +2,7 @@ const defaultState = {
   byId: {},
   allIds: []
 }
-
+// no actions to update redux store for now ): 
 const rewardReducer = (state = defaultState, action) => {
   return state;
 }
