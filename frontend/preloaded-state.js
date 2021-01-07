b20bb1b5c4bd1671eb69f9fc9ac3e57844939021
eric2523@gmail.com <eric2523@gmail.com>
2021-01-07 00:27:43 -0800

    update preloaded state to have past/present/future

--- frontend/preloaded-state.js
+++ frontend/preloaded-state.js
@@ -1,57 +1,67 @@
 export const preloadedState = {
   entities: {
-    categories: { byId: {
-      1: {
-        id: 1,
-        name: "Food & Drink"
-      },
-      2: {
-        id: 2,
-        name: "Automotive"
-      },
-      3: {
-        id: 3,
-        name: "Home"
-      }
-    }, allIds: [1, 2, 3] },
-    rewards: { byId: {
-      1: {
-        id: 1,
-        name: "100% OFF"
-      },
-      2: {
-        id: 2,
-        name: "$0.01 OFF"
-      }, 
-      3: {
-        id: 3,
-        name: "BOGO"
-      }
-    }, allIds: [1, 2, 3] },
-    categoryRewards: {
+    categories: {
       byId: {
         1: {
           id: 1,
-          categoryId: 1,
-          rewardId: 1
+          name: "Food & Drink",
         },
         2: {
           id: 2,
-          categoryId: 1,
-          rewardId: 2
+          name: "Automotive",
         },
         3: {
           id: 3,
-          categoryId: 2,
-          rewardId: 1
+          name: "Home",
         },
-        4: {
-          id: 4,
-          categoryId: 2,
-          rewardId: 2
-        }
       },
-      allIds: [1, 2, 3, 4]
-    }
-  }
-}
+      allIds: [1, 2, 3],
+    },
+    rewards: {
+      byId: {
+        1: {
+          id: 1,
+          name: "100% OFF",
+        },
+        2: {
+          id: 2,
+          name: "$0.01 OFF",
+        },
+        3: {
+          id: 3,
+          name: "BOGO",
+        },
+      },
+      allIds: [1, 2, 3],
+    },
+    categoryRewards: {
+      past: [],
+      future: [],
+      present: {
+        byId: {
+          1: {
+            id: 1,
+            categoryId: 1,
+            rewardId: 1,
+          },
+          2: {
+            id: 2,
+            categoryId: 1,
+            rewardId: 2,
+          },
+          3: {
+            id: 3,
+            categoryId: 2,
+            rewardId: 1,
+          },
+          4: {
+            id: 4,
+            categoryId: 2,
+            rewardId: 2,
+          },
+        },
+        allIds: [1, 2, 3, 4],
+      },
+    },
+  },
+};
