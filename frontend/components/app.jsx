2651c823339df43235e2fe80d9c4e29c3f0af863
eric2523@gmail.com <eric2523@gmail.com>
2021-01-07 16:10:51 -0800

    add more styling

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,16 +1,29 @@
-import React from "react"
-import TableIndex from "./table-index"
-import HistoryButtons from "./history-buttons"
+import React from "react";
+import TableIndex from "./table-index";
+import HistoryButtons from "./history-buttons";
 
 const App = () => {
   return (
     <div>
       <TableIndex />
       <HistoryButtons />
+      <div className="socials">
+        <a
+          href="https://github.com/eric2523"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src="https://img.icons8.com/clouds/100/000000/github.png" />
+        </a>
+        <a
+          href="https://www.linkedin.com/feed/"
+          target="_blank"
+          rel="noopener noreferrer"
+        ></a>
+        <img src="https://img.icons8.com/clouds/100/000000/linkedin.png" />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-
-
-export default App
+export default App;
