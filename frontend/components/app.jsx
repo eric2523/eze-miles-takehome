b8608a4b67335f2faf8feced7c8da8335afd69f5
eric2523@gmail.com <eric2523@gmail.com>
2021-01-07 16:36:01 -0800

    update links

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -9,18 +9,19 @@ const App = () => {
       <HistoryButtons />
       <div className="socials">
         <a
-          href="https://github.com/eric2523"
+          href="https://github.com/eric2523/eze-miles-takehome"
           target="_blank"
           rel="noopener noreferrer"
         >
           <img src="https://img.icons8.com/clouds/100/000000/github.png" />
         </a>
         <a
-          href="https://www.linkedin.com/feed/"
+          href="https://www.linkedin.com/in/eric-xian-2998061a6/"
           target="_blank"
           rel="noopener noreferrer"
-        ></a>
-        <img src="https://img.icons8.com/clouds/100/000000/linkedin.png" />
+        >
+          <img src="https://img.icons8.com/clouds/100/000000/linkedin.png" />
+        </a>
       </div>
     </div>
   );
